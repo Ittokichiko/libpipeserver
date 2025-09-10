cf5bb265d1e6a9fe03a811eb2659024da54d9611
CheeZe Dark(Rikko Matsumato) <110032482+CheeZeDark@users.noreply.github.com>
2025-09-10 19:53:25 +0300

    Add files via upload

---
+++ libpipeserver/pipe_manager.h
@@ -0,0 +1,42 @@
+﻿#pragma once
+#include <Windows.h>
+#include <string>
+#include <iostream>
+#include <unordered_map>
+#include <thread>
+#include <mutex>
+#include <optional>
+#include "logger.h"
+class PipeManager
+{
+public:
+    static PipeManager& getInstance();
+    static bool isUsingPipes();
+
+    void start();
+    void stop();
+
+    bool isFeatureEnabled(const std::string& featureName) const;
+    void setFeatureState(const std::string& featureName, bool enabled);
+    void runServer();
+    void handleClient(HANDLE hPipe);
+    // Prevent copying
+    PipeManager(const PipeManager&) = delete;
+    PipeManager& operator=(const PipeManager&) = delete;
+
+    // Prevent moving
+    PipeManager(PipeManager&&) = delete;
+    PipeManager& operator=(PipeManager&&) = delete;
+
+private:
+    PipeManager();
+    ~PipeManager();
+
+    std::atomic<bool> m_running{false};
+    std::thread m_serverThread;
+    std::unordered_map<std::string, bool> m_features;
+    mutable std::mutex m_featuresMutex;
+
+    static constexpr auto PIPE_NAME = L"\\\\.\\pipe\\JMNX0034";
+    static constexpr auto PIPE_FLAG_NAME = "UsePipeModeFlag_";
+};
